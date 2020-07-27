1a13abd57834277a3ee61037825ead99da0dd883
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2020-07-27 17:11:07 -0600

    Testing creating equation

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+def getEquation(coordinate1, coordinate2):
+    m = ((int(coordinate1[1]) - int(coordinate2[1])) / (int(coordinate1[0]) - int(coordinate2[0])))
+    print(m * int((coordinate1[0])) + int(coordinate1[1]))
+
+
+Coord1A = "1,1".split(",")
+Coord1B = "3,3".split(",")
+
+getEquation(Coord1A, Coord1B)
+
+Coord2A = "-6,0".split(",")
+Coord2B = "-4,-2".split(",")
+
+
 line1M = 1
 line1B = 2
 
