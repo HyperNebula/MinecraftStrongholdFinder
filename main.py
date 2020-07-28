e2c0e7f2efd9f34943eb92884bd31658458856fc
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2020-07-28 11:30:19 -0600

    Fixed equation creater and solver

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 def getEquation(coordinate1, coordinate2):
     m = ((int(coordinate1[1]) - int(coordinate2[1])) / (int(coordinate1[0]) - int(coordinate2[0])))
-    print(m * int((coordinate1[0])) + int(coordinate1[1]))
+    b = m * -int((coordinate1[0])) + int(coordinate1[1])
+    return (m,b)
 
 
 Coord1A = "1,1".split(",")
 Coord1B = "3,3".split(",")
 
-getEquation(Coord1A, Coord1B)
+(line1M,line1B) = getEquation(Coord1A, Coord1B)
 
-Coord2A = "-6,0".split(",")
-Coord2B = "-4,-2".split(",")
+Coord2A = "0,6".split(",")
+Coord2B = "6,0".split(",")
 
-
-line1M = 1
-line1B = 2
-
-line2M = -1
-line2B = -6
+(line2M,line2B) = getEquation(Coord2A, Coord2B)
 
 AnsX = ((line1B - line2B) / (line2M - line1M))
 AnsY = (line1M * AnsX + line1B)
